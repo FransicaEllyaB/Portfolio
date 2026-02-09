c0e2c634c068e0a94b5e633e658cc5fce1e82a3d
fransiscaellyabunaren <ellyafransiscab@gmail.com>
2026-02-09 18:55:27 +0700

    Modify the UI and make the connect section

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
 const sidebar = document.querySelector('.sidebar');
 const theme = localStorage.getItem("theme");
+const spinner = document.getElementById("spinner");
+const sendButton = document.getElementById("send-button");
 const element = document.body;
 
 hamburger.addEventListener('click', () => {
@@ -50,4 +52,29 @@ function switchTheme() {
 if (theme === "dark"){
   element.classList.add("dark-mode");
   document.getElementById("slide-box").checked = true;
+}
+
+function sendMail() {
+  let parms = {
+    name : document.getElementById("name").value,
+    email : document.getElementById("email").value,
+    subject : document.getElementById("subject").value,
+    message : document.getElementById("message").value
+  }
+  spinner.style.display = "inline-block";
+
+  emailjs.send("service_6g69p4i", "template_nhj6v49", parms)
+  .then((response) => {
+    console.log('SUCCESS!', response.status, response.text);
+    spinner.style.display = "none";
+  })
+  .catch((error) => {
+    console.log('FAILED....', error);
+    spinner.style.display = "none";
+  });
+}
+
+function dummy() {
+  console.log("dummy");
+  sendButton.disabled = true;
 }
